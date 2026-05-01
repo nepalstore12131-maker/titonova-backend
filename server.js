43a725ba36bdd78280c154e0d7d5cb51e556a25d
k2z <nepalstore12131@gmail.com>
2026-05-01 01:03:35 -0500

    Configure CORS to allow all origins

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}))
 app.use(express.json());
 
 app.get("/", (req, res) => {
